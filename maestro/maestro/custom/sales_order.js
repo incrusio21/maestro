28f3bbbc07357ef6171e5f7481f59cf933705544
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-10-29 03:57:53 +0000

    upd (sales order): custom nilai entites = date_diff(check_out, check_in)

--- maestro/maestro/custom/sales_order.js
+++ maestro/maestro/custom/sales_order.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Sales Order Item", {
+    custom_check_in(frm, cdt, cdn) {
+        frm.trigger("set_enteties", cdt, cdn);
+    },
+    custom_check_out(frm, cdt, cdn) {
+        frm.trigger("set_enteties", cdt, cdn);
+    },
 	custom_quantity(frm, cdt, cdn) {
         var item = locals[cdt][cdn]
         var real_qty = item.custom_quantity
@@ -10,5 +16,19 @@ frappe.ui.form.on("Sales Order Item", {
         }
 
         frappe.model.set_value(cdt, cdn, "qty", flt(real_qty, precision("qty", item)))
+    },
+    set_enteties(frm, cdt, cdn){
+        var item = locals[cdt][cdn]
+        
+        if(!item.custom_check_in || !item.custom_check_out) return
+
+        check_in = moment(item.custom_check_in).format(frappe.defaultDateFormat)
+        check_out = moment(item.custom_check_out).format(frappe.defaultDateFormat)
+
+        frappe.model.set_value(cdt, cdn, "custom_entities", flt(
+            frappe.datetime.get_diff(item.custom_check_out, check_in), precision("custom_entities", item)))
+
+        frm.trigger("custom_quantity", cdt, cdn);
     }
+
 })
--- maestro/maestro/custom/sales_order.py
+++ maestro/maestro/custom/sales_order.py
@@ -1,16 +1,16 @@
 # Copyright (c) 2024, DAS and Contributors
 # License: GNU General Public License v3. See license.txt
 
-from frappe.utils import flt
+from frappe.utils import date_diff, flt
 
 def real_qty_items(self, method=None):
     for i in self.items:
         if not i.custom_quantity:
             i.custom_quantity = i.qty
-            continue
         
         real_qty = i.custom_quantity
         if i.item_group in ["Hotels"]:
+            i.custom_entities = date_diff(i.custom_check_out, i.custom_check_in) or 1
             real_qty *= i.custom_entities
 
         i.qty = flt(real_qty, i.precision("qty"))
